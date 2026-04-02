242d63916461fb994dff974805f0d5a2ceb6e90a
emb-ilyes <84541330+emb-ilyes@users.noreply.github.com>
2026-04-02 17:17:52 +0100

    Remove icon property from App.js

--- App.js
+++ App.js
@@ -4,14 +4,15 @@
     "slug": "quit-smoke-app",
     "version": "1.0.0",
     "orientation": "portrait",
-    "icon": "./assets/icon.png",
     "userInterfaceStyle": "dark",
     "android": {
       "package": "com.yourname.quitsmoke",
       "adaptiveIcon": {
         "backgroundColor": "#020205"
       }
+    },
+    "ios": {
+      "supportsTablet": true
     }
   }
 }
-
